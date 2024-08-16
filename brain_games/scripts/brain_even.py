87eba2190a6884ade489e5a2f69fc81090407578
krotkikhmaxim <krotkikhmaxim@yandex.ru>
2024-08-16 17:41:24 +0500

    add cache

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from brain_games import function_set
+
+def main():
+    function_set.main()
+    function_set.welcome()
+    function_set.logic_brain_even()
+
+if __name__ == '__main__':
+    main()
