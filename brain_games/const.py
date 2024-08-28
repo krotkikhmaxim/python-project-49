4581f656781bcf7daec3eda94986e7b6464a4600
krotkikhmaxim <krotkikhmaxim@yandex.ru>
2024-08-28 16:09:07 +0500

    add const

---
+++ brain_games/const.py
@@ -0,0 +1,13 @@
+task_prime = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+task_gcd = 'Find the greatest common divisor of given numbers.'
+
+task_progression = 'What number is missing in the progression?'
+
+task_even = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+task_calc = 'What is the result of the expression?'
+
+welcome = 'Welcome to the Brain Games!'
+
+iterations = 3
